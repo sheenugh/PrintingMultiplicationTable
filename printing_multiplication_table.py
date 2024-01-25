27289ca658e0a1359a7bf0acdebc52002dd9baab
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-25 14:28:51 +0800

    changes lang sa paggamit ng words and such characters sa comments

--- printing_multiplication_table.py
+++ printing_multiplication_table.py
@@ -1,8 +1,8 @@
 
 
-# ----- PSEUDO CODE -----
-# - ACTUAL CODE
-# - Using for loop, print a multiplication table
+# >>>>>>>>>> PSEUDO CODE <<<<<<<<<<
+# ----- ACTUAL CODES -----
+# - Printing a multiplication table using for loop
 for x in range(1,11):
     for y in range(1,11):
         print(x*y, end=" ")
